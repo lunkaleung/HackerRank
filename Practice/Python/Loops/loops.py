d7676689a8baa66b12badeb608a79e0fe3ef02d9
David LUN Ka Leung <P3712@cmhk.local>
2021-07-22 14:56:04 +0800

    Update

---
+++ Practice/Python/Loops/loops.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    n = int(input())
+
+    if(n >= 0):
+        i = 0
+
+        for i in range(n):
+            print(i * i)
---
+++ Practice/Python/Write a function/write_a_function.py
@@ -0,0 +1,11 @@
+def is_leap(year):
+    leap = False
+    
+    # Write your logic here
+    if year >= 1990 and year <= 10**5:
+        if year % 4 == 0:
+            if year % 100 != 0 or year % 400 == 0:
+                leap = True
+    
+    return leap
+
