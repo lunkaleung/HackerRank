3dd69f45cb753245a6c9ddd1768170bb46f02d5f
David LUN Ka Leung <P3712@cmhk.local>
2021-10-29 08:57:54 +0800

    Create lists.py

---
+++ Practice/Python/Lists/lists.py
@@ -0,0 +1,19 @@
+#def main(list):
+
+
+
+if __name__ == '__main__':
+    N = int(input())
+    
+    list = []
+    
+    while N > 0:
+        cmd, index, element = input().split()
+        
+        if cmd == "insert":
+        elif cmd == "append":
+        elif cmd == "remove":
+        elif cmd == ""
+        
+        N = N - 1
+    
